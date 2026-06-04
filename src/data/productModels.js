be7f542adebfa1af9c3eedfe9396bff65be4ba8e
Yogesh <yogeshvidhauliya130@gmail.com>
2026-06-04 17:50:52 +0530

    update

--- src/data/productModels.js
+++ src/data/productModels.js
@@ -343,15 +343,9 @@ export const PRODUCT_MODELS = {
   // ── Passive Products ────────────────────────
   "Passive Products": {
     "Unarmoured Fiber Cable": ["SY-CATV-2F-UNARM-GYP( Coil)", "SY-CATV-4F-UNARM-GYP ( Coil)", "SY-CATV-6F-UNARM-GYP( Coil)", "SY-CATV-12F-UNARM-GYP( Coil)"],
-<<<<<<< HEAD
-    "Armoured Fiber Cable": ["SY-CATV-6F-ARM-OM2", "SY-CATV-12F-ARM-OM2", "SY-CATV-6F-ARM-OM3", "SY-CATV-12F-ARM-OM3","other"],
-    "Drop Cable": ["Syrotech-DROP2FR-A1-IND", "SY-DROP2FF-A1-IND", "SY-DROP1FF-A1-IND"],
-    "CAT6 Cable": ["SY-CAT6-UTP-305M(0.48)", "SY-CAT6-SFTP-305M (0.51)", "SY-CAT6-SFTP-305M (0.55)"],
-=======
-    "Armoured Fiber Cable": ["SY-CATV-6F-ARM-OM2", "SY-CATV-12F-ARM-OM2", "SY-CATV-6F-ARM-OM3", "SY-CATV-12F-ARM-OM3","SY-CATV-2F-ARM-GYP", "SY-CATV-4F-ARM-GYP","SY-CATV-6F-ARM-GYP", "SY-CATV-12F-ARM-GYP","SY-CATV-24F-ARM-GYP"],
+    "Armoured Fiber Cable": ["SY-CATV-6F-ARM-OM2", "SY-CATV-12F-ARM-OM2", "SY-CATV-6F-ARM-OM3", "SY-CATV-12F-ARM-OM3","SY-CATV-2F-ARM-GYP", "SY-CATV-4F-ARM-GYP","SY-CATV-6F-ARM-GYP", "SY-CATV-12F-ARM-GYP","SY-CATV-24F-ARM-GYP","other"],
     "Drop Cable": ["SY-DROP2FR-A1-IND", "SY-DROP2FF-A1-IND", "SY-DROP1FF-A1-IND"],
     "CAT6 Cable": ["SY-CAT6-UTP-305M-23A  ", "SY-CAT6-UTP-305M-24A", "SY-CAT6-SFTP-305M-24A", " SY-CAT6-SFTP-305M-23A"],
->>>>>>> 23740742420141b9d3a43d7258960ff09da6175e
     "Adaptor": [
       "SY-FA-SCPC",
       "SY-FA-SCAPC",
