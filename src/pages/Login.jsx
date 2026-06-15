beaf4602b885cdbe03a13f69f529399f7e078198
Yogesh <yogeshvidhauliya130@gmail.com>
2026-06-15 17:48:19 +0530

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -270,7 +270,7 @@ const INDIAN_STATES_LOGIN = Object.keys(STATE_CITY_MAP).sort();
 
     {/* LEFT SIDE — Banner Image */}
     <div className="login-left-panel">
-      <img src="/banner2.jpg" alt="Syrotech Banner" />
+      <img src="/banner.png" alt="Syrotech Banner" />
     </div>
 
     {/* RIGHT SIDE — Login Card */}
